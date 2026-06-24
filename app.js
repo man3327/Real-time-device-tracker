7deb4ff9937ea31ab855418e6d46702af52cb482
Manvendra Singh <manvendras087@gmail.com>
2026-06-24 18:02:25 +0530

    Map and Markers added for multiple devices

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+//We need a http server for socketio
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+io.on("connection", function (socket) {
+    socket.on("send-location",function (data){
+        io.emit("receive-location",{id:socket.id,...data});
+    })
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id);
+    })
+});
+app.get("/", function (req, res) {
+    res.render("index");
+});
+server.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000');
+});
